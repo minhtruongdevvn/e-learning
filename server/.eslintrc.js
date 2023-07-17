c621424288fbbe8dcbcaa9728f08ed2fff8d4749
Minh Truong <minhtruongdevvn@gmail.com>
2023-07-17 08:54:52 +0700

    initial commit

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: 'server/tsconfig.json',
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    'prettier'
+    'prettier',
   ],
   root: true,
   env: {
